72b1e79fb60efb33f93bef6365009ad02760ffc1
Jordan Irwin <antumdeluge@gmail.com>
2017-08-11 15:23:14 -0700

    Update LDoc configuration file & generation script

--- docs/gendoc.sh
+++ docs/gendoc.sh
@@ -2,9 +2,14 @@
 
 DOCS="$(dirname $(readlink -f $0))"
 ROOT="$(dirname ${DOCS})"
-
 CONFIG="${DOCS}/config.ld"
-OUT="${DOCS}"
 
 cd "${ROOT}"
-ldoc -c "${CONFIG}" -d "${OUT}" "${ROOT}/api.ld"
+
+# Clean old files
+rm -rf "${DOCS}/api.html" "${DOCS}/scripts" "${DOCS}/modules"
+# Create new files
+ldoc -c "${CONFIG}" -d "${DOCS}" "${DOCS}"
+
+# Put "prefix:name_" settings in angled brackets (<>)
+sed -i -e 's|>prefix:name|>\&lt;prefix\&gt;:\&lt;name\&gt;|' "${DOCS}/api.html"
